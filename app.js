242432c7b8dd614f675a5b37b569d6d6fc3e775b
Joe Yang <jy22yang@uwaterloo.ca>
2017-05-26 23:25:37 -0400

    yeah

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 // This example adds a red rectangle to a map.
 
+$(document).ready(function(){
+  $("peek").click(function(){
+    console.log("asdf")
+  });
+});
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {lat: 33.678, lng: -116.243},
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 43.476, lng: -80.536},
     mapTypeId: 'terrain'
   });
+  dots = {}
+}
 
-  var rectangle = new google.maps.Rectangle({
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    map: map,
-    bounds: {
-      north: 33.685,
-      south: 33.671,
-      east: -116.234,
-      west: -116.251
+function sendLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+          colour:
+        };
+        $.post("route/pixels", pos);
+      }, function() {
+        console.log("LOCATION DIDNT WORK")
+      });
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function getDots() {
+  $.ajax({
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'json',
+    url:'https://our-place.herokuapp.com/pixels',
+    success: function(data) {
+      $.each(data, function(index, element) {
+        console.log(element)
+      });
     }
   });
 }
-$(document).ready(function(){
-  $("#peek").click(function(){
-    console.log('asdf');
-  })  
-})
